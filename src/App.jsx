c013855938660c90605beed507a21d2fcaed6518
GarrixA <aphrogarrix10@gmail.com>
2023-10-13 07:07:42 +0200

    comp

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import './App.css'
 import Navbar from './reusable/Navbar'
 import './index.css'
 
+
+
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -11,6 +14,8 @@ function App() {
     <>
     <div>
     <Navbar/>
+    
+    
     </div>
     </>
   )
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
 import './menu.css'
+import {TbMenuDeep} from 'react-icons/tb'
 
 function Menu() {
     return (
         <div>
             <input type="checkbox" id="active"/>
-            <label for="active" class="menu-btn"><span></span></label>
+            <label for="active" class="menu-btn"><span><TbMenuDeep size="40px" color='#fff'/> </span></label>
             <label for="active" class="close"></label>
             <div class="wrapper">
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Services</li>
-                    <li>Gallery</li>
-                    <li>Feedback</li>
-                </ul>
+                
+                <div className='logo'>
+
+                </div>
+                <div className="menu-list">
+                    
+                </div>
             </div>
+            
         </div>
     )
 }
